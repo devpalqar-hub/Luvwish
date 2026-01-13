4df46d747b4a30b6f47049e34e0e86ef9f29c0bb
AK-BuNNy <ananthakrishnan3644@gmail.com>
2026-01-13 14:53:33 +0530

    feat: new tracking module

--- src/tracking/tracking-detail.module.ts
+++ src/tracking/tracking-detail.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { TrackingDetailService } from './tracking-detail.service';
 import { TrackingDetailController } from './tracking-detail.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { TrackingModule } from './tracking.module';
+import { TrackingController } from './tracking.controller';
+import { TrackingService } from './tracking.service';
 
 @Module({
-    controllers: [TrackingDetailController],
-    providers: [TrackingDetailService, PrismaService],
-    exports: [TrackingDetailService], // export if other modules need it
+    controllers: [TrackingDetailController, TrackingController],
+    providers: [TrackingDetailService, PrismaService, TrackingService],
+    exports: [TrackingDetailService],
+    imports: [TrackingModule], // export if other modules need it
 })
 export class TrackingDetailModule { }
