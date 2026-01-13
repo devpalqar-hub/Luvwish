0ff84ffd148f80d8f6944ca750adf6ca774e4b5f
AK-BuNNy <ananthakrishnan3644@gmail.com>
2026-01-13 18:26:08 +0530

    feat: added categories with image upload added user cancel

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Module } from '../s3/s3.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, S3Module],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
