0ea9a9886b52d7109f71897be696018ba1de9943
AK-BuNNy <ananthakrishnan3644@gmail.com>
2026-01-13 19:43:25 +0530

    fix: category image upload fix

--- src/subcategories/subcategories.module.ts
+++ src/subcategories/subcategories.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SubCategoriesService } from './subcategories.service';
 import { SubCategoriesController } from './subcategories.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Module } from '../s3/s3.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, S3Module],
   controllers: [SubCategoriesController],
   providers: [SubCategoriesService],
   exports: [SubCategoriesService],
