a59a971bf66cf46cb218779d5dee150042a4516b
AK-BuNNy <ananthakrishnan3644@gmail.com>
2026-05-13 10:59:33 +0530

    fix 4

---
+++ prisma/migrations/20260513052926_added_mock_reviews/migration.sql
@@ -0,0 +1,20 @@
+-- DropForeignKey
+ALTER TABLE `review` DROP FOREIGN KEY `Review_customerProfileId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `review` DROP FOREIGN KEY `Review_orderItemId_fkey`;
+
+-- DropIndex
+DROP INDEX `Review_customerProfileId_fkey` ON `review`;
+
+-- AlterTable
+ALTER TABLE `review` ADD COLUMN `isMock` BOOLEAN NOT NULL DEFAULT false,
+    ADD COLUMN `mockReviewerName` VARCHAR(191) NULL,
+    MODIFY `customerProfileId` VARCHAR(191) NULL,
+    MODIFY `orderItemId` VARCHAR(191) NULL;
+
+-- AddForeignKey
+ALTER TABLE `Review` ADD CONSTRAINT `Review_customerProfileId_fkey` FOREIGN KEY (`customerProfileId`) REFERENCES `CustomerProfile`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Review` ADD CONSTRAINT `Review_orderItemId_fkey` FOREIGN KEY (`orderItemId`) REFERENCES `order_items`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
