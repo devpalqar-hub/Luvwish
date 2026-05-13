d96dea01d16aa103ad7471b81d18dd43576ab80f
AK-BuNNy <ananthakrishnan3644@gmail.com>
2026-05-13 11:10:50 +0530

    migratin fix

--- prisma/migrations/20260513052926_added_mock_reviews/migration.sql
+++ prisma/migrations/20260513052926_added_mock_reviews/migration.sql
@@ -1,20 +1,33 @@
 -- DropForeignKey
-ALTER TABLE `review` DROP FOREIGN KEY `Review_customerProfileId_fkey`;
+ALTER TABLE `Review`
+DROP FOREIGN KEY `Review_customerProfileId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `review` DROP FOREIGN KEY `Review_orderItemId_fkey`;
+ALTER TABLE `Review`
+DROP FOREIGN KEY `Review_orderItemId_fkey`;
 
 -- DropIndex
-DROP INDEX `Review_customerProfileId_fkey` ON `review`;
+DROP INDEX `Review_customerProfileId_fkey` ON `Review`;
 
 -- AlterTable
-ALTER TABLE `review` ADD COLUMN `isMock` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `mockReviewerName` VARCHAR(191) NULL,
-    MODIFY `customerProfileId` VARCHAR(191) NULL,
-    MODIFY `orderItemId` VARCHAR(191) NULL;
+ALTER TABLE `Review`
+ADD COLUMN `isMock` BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN `mockReviewerName` VARCHAR(191) NULL,
+MODIFY `customerProfileId` VARCHAR(191) NULL,
+MODIFY `orderItemId` VARCHAR(191) NULL;
 
 -- AddForeignKey
-ALTER TABLE `Review` ADD CONSTRAINT `Review_customerProfileId_fkey` FOREIGN KEY (`customerProfileId`) REFERENCES `CustomerProfile`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Review`
+ADD CONSTRAINT `Review_customerProfileId_fkey`
+FOREIGN KEY (`customerProfileId`)
+REFERENCES `CustomerProfile`(`id`)
+ON DELETE SET NULL
+ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Review` ADD CONSTRAINT `Review_orderItemId_fkey` FOREIGN KEY (`orderItemId`) REFERENCES `order_items`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Review`
+ADD CONSTRAINT `Review_orderItemId_fkey`
+FOREIGN KEY (`orderItemId`)
+REFERENCES `order_items`(`id`)
+ON DELETE SET NULL
+ON UPDATE CASCADE;
